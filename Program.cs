5676966a5a1bc5d1dcb1ca6a2490c1b5eeb281fc
eldar23451 <liyev8024@gmail.com>
2024-05-22 21:48:05 +0400

    paramstask

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+//int SumNumbers(  params     int[] array)
+//{
+//    int sum = 0;
+//    for (int i = 0; i < array.Length; i++)
+//    {
+
+//        sum += array[i];
+
+
+
+//    }
+//    return sum;
+
+
+
+
+
+
+//}Console.WriteLine(SumNumbers(new int[] { 10, 22, 36, 23, 65, 13, 15, 18 }));
+//Console.WriteLine(SumNumbers(10, 22, 36, 23, 65, 13, 15, 18));
+
+
+
+
+
